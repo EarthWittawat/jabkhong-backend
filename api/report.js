22a40522eaea6d82d676ecd0d9e51092f6a219d5
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-10 18:34:32 +0700

    add api

---
+++ api/report.js
@@ -0,0 +1,21 @@
+const express = reqiuire("express");
+const router = express.Router();
+
+const connection = mysql.createConnection({
+    host: 'jabkhong.ckj2cg17krub.us-east-1.rds.amazonaws.com',
+    port:  '3306',
+    user: 'admin',
+    password: 'jabkhong2023',
+    database: 'jabkhong'
+    });
+
+router.get('/report', function (req, res, next) {
+    connection.query(
+      'SELECT * FROM `report`',
+      function(err, results, fields) {
+        res.end(results);
+      }
+    );
+  });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const app = require('express')();
-const { v4 } = require('uuid');
-
 const mysql = require('mysql2')
+const report = require("./api/report");
 
-const connection = mysql.createConnection({
-host: 'jabkhong.ckj2cg17krub.us-east-1.rds.amazonaws.com',
-port:  '3306',
-user: 'admin',
-password: 'jabkhong2023',
-database: 'jabkhong'
-});
+app.use(express.json({extends: false}));
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-app.get('/report', function (req, res, next) {
-    connection.query(
-      'SELECT * FROM `report`',
-      function(err, results, fields) {
-        res.end(results);
-      }
-    );
-  });
+app.use("/api/report",report);
+const PORT = process.env.port || 8000;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
 module.exports = app;
