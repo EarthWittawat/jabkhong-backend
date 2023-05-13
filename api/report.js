26631ae9c2a737c1f5820291a47279d921a1aed0
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-13 22:47:54 +0700

    env variables

--- api/report.js
+++ api/report.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host: 'jabkhong.ckj2cg17krub.us-east-1.rds.amazonaws.com',
-    port:  '3306',
-    user: 'admin',
-    password: 'jabkhong2023',
-    database: 'jabkhong'
-    });
+  host: process.env.HOST,
+  port:   process.env.PORT,
+  user:  process.env.USER,
+  password:  process.env.PASSWORD,
+  database:  process.env.DATABASE
+  });
+
 
 router.get('/', async (req, res) => {
     try{
