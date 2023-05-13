68284e1381ba9d29fa1580f69d940a23a499a12e
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-13 20:30:19 +0700

    reportrequest api

--- api/reportrequest.js
+++ api/reportrequest.js
@@ -10,19 +10,29 @@ const connection = mysql.createConnection({
     database: 'jabkhong'
     });
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try{
     connection.query(
       'SELECT * FROM `report`',
       function(err, results, fields) {
-        res.json(results);
-      }
-    );
+        const filters = req.query;
+        const filterdUsers = results.filter(search => {
+          let isValid = true;
+          for (key in filters){
+            console.log(key, search[key], filters[key]);
+            isValid = isValid && search[key] == filters[key];
+          }
+          return isValid;
+        });
+        res.send(filterdUsers);        
+      });
     } catch(error){
         console.error(error);
         return res.status(500).send("Server error");
     }
-});
+  
+}
+);
 
 
 module.exports = router
--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const app = express();
 const report = require("./api/report");
 const user = require("./api/user");
+const reportrequest = require("./api/reportrequest");
 app.use(express.json({extends: false}));
 app.use(cors());
 
 app.use("/api/report",report);
 app.use("/api/user",user);
+app.use("/api/reportrequest",reportrequest);
 const PORT = process.env.port || 8000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
