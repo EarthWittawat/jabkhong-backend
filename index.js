cc7f0f8c7704f3a72487945f4faa55896d892970
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-14 19:06:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const user = require("./api/user");
 const reportrequest = require("./api/reportrequest");
 const register = require("./api/register");
 app.use(express.json({extends: false}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://jabkhong.vercel.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(cors());
-
 app.use("/api/report",report);
 app.use("/api/user",user);
 app.use("/api/reportrequest",reportrequest);
