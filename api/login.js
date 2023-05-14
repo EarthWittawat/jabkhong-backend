ba73dbaad746c5c59b882098678ec8c4d77f7e84
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-14 19:25:38 +0700

    login api

---
+++ api/login.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  port:   process.env.PORT,
+  user:  process.env.USER,
+  password:  process.env.PASSWORD,
+  database:  process.env.DATABASE
+  });
+  router.get('/', async (req, res) => {
+    try{
+    connection.query(
+      'SELECT * FROM `users`',
+      function(err, results, fields) {
+        res.json(results);
+      }
+    );
+    } catch(error){
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+});
+router.post('/', (req, res) => {
+    const id_card = req.body.id_card
+    const tel = req.body.tel
+    try{
+    connection.query(
+      "SELECT * FROM `users` WHERE id_card = ? AND tel = ?",
+      [id_card, tel],
+      function(err, results, fields) {
+        res.send(results);
+      }
+    );
+    } catch(error){
+        console.error(error);
+        return res.send({message : "Wrong Username/Password"})
+    }
+});
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const report = require("./api/report");
 const user = require("./api/user");
 const reportrequest = require("./api/reportrequest");
 const register = require("./api/register");
+const login = require("./api/login");
 app.use(express.json({extends: false}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://jabkhong.vercel.app"); // update to match the domain you will make the request from
@@ -16,6 +17,7 @@ app.use("/api/report",report);
 app.use("/api/user",user);
 app.use("/api/reportrequest",reportrequest);
 app.use("/api/register",register);
+app.use("/api/login",login);
 const PORT = process.env.port || 8000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
