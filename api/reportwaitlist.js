b20d115174d70858b069e569230979bf33e7bf0a
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-15 00:15:48 +0700

    Update reportwaitlist.js

--- api/reportwaitlist.js
+++ api/reportwaitlist.js
@@ -24,6 +24,24 @@ router.get('/', async (req, res) => {
         return res.status(500).send("Server error");
     }
 });
-
+router.post('/', (req, res) => {
+    const name = req.body.name
+    const details = req.body.details
+    const bank = req.body.bank
+    const promptpay = req.body.promptpay
+    const truewallet = req.body.truewallet
+    try{
+    connection.query(
+      "INSERT INTO `report_waitlist` (name, details, bank, promptpay, truewallet) VALUES (?,?,?,?,?)",
+      [name, details, bank, promptpay, truewallet],
+      function(err, results, fields) {
+        res.json(results);
+      }
+    );
+    } catch(error){
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+});
 
 module.exports = router
