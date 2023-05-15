424b20809248d66f1a37406d27a33e74289a285c
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-15 15:00:15 +0700

    cors fix

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const login = require("./api/login");
 const report_waitlist = require("./api/reportwaitlist");
 app.use(express.json({extends: false}));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://jabkhong.vercel.app"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
 app.use(cors());
